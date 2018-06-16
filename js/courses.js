8025dda02b7a964d8054946782fa9b65da0591af
devakrishna33 <36110228+devakrishna33@users.noreply.github.com>
2018-06-16 12:49:50 +0530

    corrected media scalabilities

--- js/courses.js
+++ js/courses.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
+  var prevScrollpos = window.pageYOffset;
   $("#myInput").on("keyup", function() {
     var value = $(this).val().toLowerCase();
     $(".dropdown-menu li").filter(function() {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
-
+  if ($(window).width() <= 380) {  
+        $(".navbar-brand").hide();
+        $(".jumbotron").show();
+        $(".jumbotron").css("width",$(window).width());
+}
   $("#departments li").click(function() {
   	var active = $(".course-active");
   	active.hide();
@@ -15,7 +20,18 @@ $(document).ready(function(){
   	$('#'+id).show();
   	 $('#'+id).addClass('course-active');
   	 $('#courses').html(depart);
-     $(".footer").css({"margin-top":"0px"})
   }
   	);
+
+
+  $(window).scroll(function() {
+    var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+});
+
 });
--- js/placements.js
+++ js/placements.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
+  var prevScrollpos = window.pageYOffset;
   $("#myInput").on("keyup", function() {
     var value = $(this).val().toLowerCase();
     $(".dropdown-menu li").filter(function() {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
-
+  if ($(window).width() <= 380) {  
+        $(".navbar-brand").hide();
+        $(".jumbotron").show();
+        $(".jumbotron").css("width",$(window).width());
+}
   $("#departments li").click(function() {
   	var active = $(".course-active");
   	active.hide();
@@ -18,4 +23,14 @@ $(document).ready(function(){
      $(".footer").css({"margin-top":"0px"})
   }
   	);
+
+    $(window).scroll(function() {
+    var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+});
 });
